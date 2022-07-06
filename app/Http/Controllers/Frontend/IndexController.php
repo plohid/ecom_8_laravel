d9dca1c87c0b086974994b69a3f2b496e5cd4f13
plohid <ohid91@gmail.com>
2022-07-06 16:07:45 +0600

    frontend template setup

---
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    //index page
+
+    public function index(){
+        return view('Frontend.index');
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\User\UserController;
 Use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Frontend\IndexController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,19 +17,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+// =============================== admin route  ===========================================
 
 Route::group(['prefix'=>'admin','middleware' =>['admin','auth'],'namespace'=>'Admin'], function(){
     Route::get('dashboard',[AdminController::class,'index'])->name('admin.dashboard');
 });
 
+// =============================== user route  ===========================================
+
 Route::group(['prefix'=>'user','middleware' =>['user','auth'],'namespace'=>'User'], function(){
     Route::get('dashboard',[UserController::class,'index'])->name('user.dashboard');
 });
 
+// =============================== frontend route  ===========================================
+Route::get('/', [IndexController::class, 'index']);
