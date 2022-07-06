1cd5da8408b369bb36e706844887827169220d8c
plohid <ohid91@gmail.com>
2022-07-06 18:00:18 +0600

    loging page setu

--- resources/views/layouts/frontend_master.blade.php
+++ resources/views/layouts/frontend_master.blade.php
@@ -51,7 +51,7 @@
 					<li><a href="#"><i class="icon fa fa-heart"></i>Wishlist</a></li>
 					<li><a href="#"><i class="icon fa fa-shopping-cart"></i>My Cart</a></li>
 					<li><a href="#"><i class="icon fa fa-check"></i>Checkout</a></li>
-					<li><a href="#"><i class="icon fa fa-lock"></i>Login</a></li>
+					<li><a href="{{route('login')}}"><i class="icon fa fa-lock"></i>Login/Register</a></li>
 				</ul>
 			</div><!-- /.cnt-account -->
 
@@ -87,7 +87,7 @@
 				<div class="col-xs-12 col-sm-12 col-md-3 logo-holder">
 					<!-- ============================================================= LOGO ============================================================= -->
 <div class="logo">
-	<a href="home.html">
+	<a href="{{url('/')}}">
 		
 		<img src="{{asset('frontend')}}/assets/images/logo.png" alt="logo">
 
@@ -455,77 +455,14 @@
 
     @yield('content')
     
-    <!-- ============================================== BRANDS CAROUSEL ============================================== -->
-<div id="brands-carousel" class="logo-slider wow fadeInUp">
+    {{-- ================brand --}}
 
-		<div class="logo-slider-inner">	
-			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
-				<div class="item m-t-15">
-					<a href="#" class="image">
-						<img data-echo="{{asset('frontend')}}/assets/images/brands/brand1.png" src="{{asset('frontend')}}/assets/images/blank.gif" alt="">
-					</a>	
-				</div><!--/.item-->
+ 
 
-				<div class="item m-t-10">
-					<a href="#" class="image">
-						<img data-echo="{{asset('frontend')}}/assets/images/brands/brand2.png" src="{{asset('frontend')}}/assets/images/blank.gif" alt="">
-					</a>	
-				</div><!--/.item-->
+</div><!-- /.container -->
 
-				<div class="item">
-					<a href="#" class="image">
-						<img data-echo="{{asset('frontend')}}/assets/images/brands/brand3.png" src="{{asset('frontend')}}/assets/images/blank.gif" alt="">
-					</a>	
-				</div><!--/.item-->
 
-				<div class="item">
-					<a href="#" class="image">
-						<img data-echo="{{asset('frontend')}}/assets/images/brands/brand4.png" src="{{asset('frontend')}}/assets/images/blank.gif" alt="">
-					</a>	
-				</div><!--/.item-->
 
-				<div class="item">
-					<a href="#" class="image">
-						<img data-echo="{{asset('frontend')}}/assets/images/brands/brand5.png" src="{{asset('frontend')}}/assets/images/blank.gif" alt="">
-					</a>	
-				</div><!--/.item-->
-
-				<div class="item">
-					<a href="#" class="image">
-						<img data-echo="{{asset('frontend')}}/assets/images/brands/brand6.png" src="{{asset('frontend')}}/assets/images/blank.gif" alt="">
-					</a>	
-				</div><!--/.item-->
-
-				<div class="item">
-					<a href="#" class="image">
-						<img data-echo="{{asset('frontend')}}/assets/images/brands/brand2.png" src="{{asset('frontend')}}/assets/images/blank.gif" alt="">
-					</a>	
-				</div><!--/.item-->
-
-				<div class="item">
-					<a href="#" class="image">
-						<img data-echo="{{asset('frontend')}}/assets/images/brands/brand4.png" src="{{asset('frontend')}}/assets/images/blank.gif" alt="">
-					</a>	
-				</div><!--/.item-->
-
-				<div class="item">
-					<a href="#" class="image">
-						<img data-echo="{{asset('frontend')}}/assets/images/brands/brand1.png" src="{{asset('frontend')}}/assets/images/blank.gif" alt="">
-					</a>	
-				</div><!--/.item-->
-
-				<div class="item">
-					<a href="#" class="image">
-						<img data-echo="{{asset('frontend')}}/assets/images/brands/brand5.png" src="{{asset('frontend')}}/assets/images/blank.gif" alt="">
-					</a>	
-				</div><!--/.item-->
-		    </div><!-- /.owl-carousel #logo-slider -->
-		</div><!-- /.logo-slider-inner -->
-	
-</div><!-- /.logo-slider -->
-
-<!-- ============================================== BRANDS CAROUSEL : END ============================================== -->
-	</div><!-- /.container -->
 </div><!-- /#top-banner-and-menu -->
 
 
